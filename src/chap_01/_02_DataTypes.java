d719b44a38fc97527a597614d6301a4fbaebcd32
youjunsang <junsangy1234@gmail.com>
2026-01-24 15:51:46 +0900

    study

---
+++ src/chap_01/_02_DataTypes.java
@@ -0,0 +1,9 @@
+package chap_01;
+
+public class _02_DataTypes {
+    public static void main(String[] args) {
+
+    }
+
+
+}
---
+++ src/chap_01/_03_Vaildables.java
@@ -0,0 +1,9 @@
+package chap_01;
+
+public class _03_Vaildables {
+    public static void main(String[] args) {
+        String name;
+        name = "Jun";
+        System.out.println(name + "입니다");
+    }
+}
